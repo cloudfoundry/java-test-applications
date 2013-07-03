8da344e601366366ec2210e54de8883a0f4c4259
Ben Hale <bhale@gopivotal.com>
2013-07-03 13:09:09 +0100

    Change Play application to Java
    Previously, the Play application was a Scala application.  This change
    makes it a Java application.
    
    [#49696153]

--- play-application/project/Build.scala
+++ play-application/project/Build.scala
@@ -9,10 +9,10 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    jdbc,
-    anorm
+    javaCore,
+    javaJdbc,
+    javaEbean
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
