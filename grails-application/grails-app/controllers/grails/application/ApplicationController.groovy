310976255febe18792d02f24bfa8aa53cb2b01eb
Ben Hale <bhale@gopivotal.com>
2013-07-10 17:21:42 +0100

    Groovy sample application
    This change adds a Groovy application to the collection of test
    applications.
    
    [#51899505]

--- grails-application/grails-app/controllers/grails/application/ApplicationController.groovy
+++ grails-application/grails-app/controllers/grails/application/ApplicationController.groovy
@@ -4,36 +4,35 @@ import java.lang.management.ManagementFactory
 
 class ApplicationController {
 
-	static {
-		if (System.getenv()['FAIL_INIT'] != null) {
-			throw new RuntimeException('$FAIL_INIT caused initialisation to fail')
-		}
-	}
+    static {
+        if (System.getenv()['FAIL_INIT'] != null) {
+            throw new RuntimeException('$FAIL_INIT caused initialisation to fail')
+        }
+    }
 
-	def index() {
-		if (System.getenv()['FAIL_OOM'] != null) {
-			System.err.println("Exhausting heap...");
-			byte[] _ = new byte[Integer.MAX_VALUE];
-		}
+    def index() {
+        if (System.getenv()['FAIL_OOM'] != null) {
+            println "Exhausting heap..."
+            byte[] _ = new byte[Integer.MAX_VALUE]
+        }
 
-		def runtimeMxBean = ManagementFactory.getRuntimeMXBean()
-		def data = new TreeMap()
-		data["Class Path"] = runtimeMxBean.classPath.split(':')
-		data["Environment Variables"] = System.getenv()
-		data["Input Arguments"] = runtimeMxBean.inputArguments
-		data["Request Headers"] = headers
+        def runtimeMxBean = ManagementFactory.getRuntimeMXBean()
+        def data = new TreeMap()
+        data["Class Path"] = runtimeMxBean.classPath.split(':')
+        data["Environment Variables"] = System.getenv()
+        data["Input Arguments"] = runtimeMxBean.inputArguments
+        data["Request Headers"] = headers
 
-		return [data: data]
-	}
+        return [data: data]
+    }
 
-	def getHeaders() {
-		def data = new TreeMap()
+    def getHeaders() {
+        def data = new TreeMap()
 
-		for(headerName in request.headerNames) {
-			data[headerName] = request.getHeader(headerName)
-		}
+        for(headerName in request.headerNames) {
+            data[headerName] = request.getHeader(headerName)
+        }
 
-		data
-	}
-
+        data
+    }
 }
