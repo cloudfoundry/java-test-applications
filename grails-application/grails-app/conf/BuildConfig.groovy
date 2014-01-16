9f26e6ac067831d45b9a1617221eb94243158016
Ramnivas Laddad <rladdad@gopivotal.com>
2014-01-15 17:54:21 -0800

    Remove mysql driver from Grails app

--- grails-application/grails-app/conf/BuildConfig.groovy
+++ grails-application/grails-app/conf/BuildConfig.groovy
@@ -49,8 +49,8 @@ grails.project.dependency.resolution = {
     }
 
     dependencies { // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        runtime 'mysql:mysql-connector-java:5.1.24'
-        compile 'com.gopivotal.cloudfoundry.test:core:1.0.0.BUILD-SNAPSHOT' }
+        compile 'com.gopivotal.cloudfoundry.test:core:1.0.0.BUILD-SNAPSHOT' 
+    }
 
     plugins {
         // plugins for the build system only
