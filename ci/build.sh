e4c449e4558e3fe620f42602c8fea9e8ab58f3c6
Christopher Frost <cfrost@pivotal.io>
2016-07-27 13:42:45 +0100

    Log the version of Java being used

--- ci/build.sh
+++ ci/build.sh
@@ -6,5 +6,7 @@ export TERM=${TERM:-dumb}
 
 git clone java-test-applications java-test-applications-built
 
+java -version
+
 cd java-test-applications-built
 ./gradlew -Dorg.gradle.native=false build -x test
