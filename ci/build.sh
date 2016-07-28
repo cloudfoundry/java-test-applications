12365fb1fb76aa4406792aa8bba10a4578f137ff
Christopher Frost <cfrost@pivotal.io>
2016-07-28 15:57:37 +0100

    Revert "Revert "Revert "Log the version of Java being used"""
    This reverts commit 2a4fa7f40c0f5cf547db89fcf82a0e5cfdfc7049.

--- ci/build.sh
+++ ci/build.sh
@@ -6,7 +6,5 @@ export TERM=${TERM:-dumb}
 
 git clone java-test-applications java-test-applications-built
 
-java -version
-
 cd java-test-applications-built
 ./gradlew -Dorg.gradle.native=false build -x test
